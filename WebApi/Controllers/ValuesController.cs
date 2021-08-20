a0959f9366a42422e6600f5c55687886b34fbc46
ruthjo <jonas.ruth@sick.de>
2021-08-20 14:47:15 +0200

    Add project files.

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace OtelPropagation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        
+        private static readonly TextMapPropagator Propagator = Propagators.DefaultTextMapPropagator;
+
+        private readonly ILogger<ValuesController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(ILogger<ValuesController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string [] Get()
+        {
+            var activity = Activity.Current;
+
+            //Propagator.Extract(new PropagationContext(activity.Context, Baggage.Current), Request.Headers, InjectContextIntoHeader);
+            
+            return new string[] { "sensor1", "sensor2", "sensor3" };
+           
+        }
+
+        
+
+        private IEnumerable<string> InjectContextIntoHeader(IHeaderDictionary arg1, string arg2)
+        {
+            return null;
+        }
+
+       
+
+      
+
+
+
+
+    }
+}
---
+++ WebApiClient/Program.cs
@@ -0,0 +1,65 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiClient
+{
+    class Program
+    { 
+
+        private static readonly TextMapPropagator Propagator = Propagators.DefaultTextMapPropagator;
+        static async Task Main(string[] args)
+        {
+            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+
+            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddHttpClientInstrumentation()
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("WebApiClient"))
+            .AddOtlpExporter(otlpOptions =>
+                {
+                     Uri uri = new Uri("http://localhost:55680");
+                     otlpOptions.Endpoint = uri;
+                })
+            .Build();
+
+           
+
+            Console.WriteLine("press any key to continue!");
+            Console.ReadLine();
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://localhost:44309/api/Values");
+                var activity = Activity.Current;
+
+                Propagator.Inject(new PropagationContext(activity.Context, Baggage.Current), response,(HttpResponseMessage x,String y,String z)=> {
+                    x.Headers.Add(y, z);
+                });
+
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(message);
+                }
+                else
+                {
+                    Console.WriteLine($"response error code : {response.StatusCode}");
+                }
+
+            }
+
+            
+            
+            
+        }
+        
+
+    }
+}
